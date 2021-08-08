9c424532d6630c40f976814812b584c55cf5ca9f
yuuut-tw <loveeat341@gmail.com>
2021-08-08 14:04:11 +0800

    application.py revised (add textmessage & env variable)

--- application.py
+++ application.py
@@ -1,11 +1,20 @@
+import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-
-
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage)
 
 
 app = Flask(__name__)
+
+LINE_SECRET = os.getenv('LINE_SECRET')
+LINE_TOKEN = os.getenv('LINE_TOKEN')
+LINE_BOT = LineBotApi(LINE_TOKEN)
+HANDLER = WebhookHandler(LINE_SECRET)
+
 
 @app.route("/")
 def hello():
@@ -27,7 +36,20 @@ def callback():
         abort(400)
     return "OK"
 
-LINE_SECRET = "284c517b44f5909886dcf9523141a054"
-LINE_TOKEN = "U05137bec0bb83f4aff678584bb4d3cc9"
-LINE_BOT = LineBotApi(LINE_TOKEN)
-HANDLER = WebhookHandler(LINE_SECRET)
+
+# message 可以針對收到的訊息種類
+@HANDLER.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    url_dict = {
+      "TIBAME":"https://www.tibame.com/coursegoodjob/traffic_cli",
+      "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/",
+      "Youtube": "https://www.youtube.com/"}
+
+# 將要發出去的文字變成TextSendMessage
+    try:
+        url = url_dict[event.message.text.upper()]
+        message = TextSendMessage(text=url)
+    except:
+        message = TextSendMessage(text=event.message.text)
+# 回覆訊息
+    LINE_BOT.reply_message(event.reply_token, message)
