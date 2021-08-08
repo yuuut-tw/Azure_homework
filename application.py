1a4ac51e65ed552ede384bf26dabc6dcc04d058b
yuuut-tw <loveeat341@gmail.com>
2021-08-08 15:37:08 +0800

    revised application.py(handle_message)

--- application.py
+++ application.py
@@ -54,7 +54,7 @@ def handle_message(event):
             bubble = json.load(f_r)
         f_r.close()
         LINE_BOT.reply_message(event.reply_token,
-                               FlexSendMessage(alt_text="Report", contents=bubble))
+                               [FlexSendMessage(alt_text="Report", contents=bubble)])
 
     except:
         message = TextSendMessage(text=event.message.text)
