c735c4e8787765536c3c48f91e820046f6e55236
yuuut-tw <loveeat341@gmail.com>
2021-08-14 15:22:55 +0800

    Azure face recognition function test

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import datetime, timezone, timedelta
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -7,16 +8,52 @@ from linebot.models import (
     MessageEvent,
     TextMessage,
     TextSendMessage,
-    FlexSendMessage)
+    FlexSendMessage,
+    ImageMessage)
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
 
 
 app = Flask(__name__)
 
+# Configuration
+# ===============================================================
+
+# LineChat Bot
 LINE_SECRET = os.getenv('LINE_SECRET')
 LINE_TOKEN = os.getenv('LINE_TOKEN')
 LINE_BOT = LineBotApi(LINE_TOKEN)
 HANDLER = WebhookHandler(LINE_SECRET)
 
+# Computer vision
+CV_KEY = os.getenv("CV_KEY")
+CV_END = os.getenv("CV_END")
+CV_CLIENT = ComputerVisionClient(
+    CV_END, CognitiveServicesCredentials(CV_KEY)
+)
+
+# Face Recognition
+FACE_KEY = os.getenv("FACE_KEY")
+FACE_END = os.getenv("FACE_END")
+FACE_CLIENT = FaceClient(FACE_END, CognitiveServicesCredentials(FACE_KEY))
+PERSON_GROUP_ID = "tibame"
+
+# Imgur
+IMGUR_CONFIG = {
+  "IMGUR_ID": os.getenv("IMGUR_ID"),
+  "IMGUR_SECRET": os.getenv("IMGUR_SECRET"),
+  "IMGUR_ACCESS_TOKEN": os.getenv("IMGUR_ACCESS_TOKEN"),
+  "IMGUR_REFRESH_TOKEN": os.getenv("IMGUR_REFRESH_TOKEN")
+}
+
+IMGUR_CLIENT = Imgur(config=IMGUR_CONFIG)
+
+
+# APP content
+# ===============================================================
 
 @app.route("/")
 def hello():
@@ -62,15 +99,59 @@ def handle_message(event):
         message = TextSendMessage(text=event.message.text)
         LINE_BOT.reply_message(event.reply_token, message)
 
+# azure 功能
+# ================================================================
 
-    # 將要發出去的文字變成TextSendMessage
-    # try:
-    #     url = url_dict[event.message.text.upper()]
-    #     message = TextSendMessage(text=url)
-    # except:
-    #     message = TextSendMessage(text=event.message.text)
-    #
-    #
-    # # 回覆訊息
-    # LINE_BOT.reply_message(event.reply_token, message)
 
+# 定義人臉辨識函數
+def azure_face_recognition(filename):
+    PERSON_GROUP_ID = "cfb101"
+    FACE_CLIENT = FaceClient(FACE_END, CognitiveServicesCredentials(FACE_KEY))
+    img = open(filename, "r+b")  # read binary
+    detected_face = FACE_CLIENT.face.detect_with_stream(img,
+                                                        detection_model="detection_01")
+    # 多於一張臉的情況
+    if len(detected_face) != 1:
+        return ""
+    results = FACE_CLIENT.face.identify([detected_face[0].face_id],
+                                        PERSON_GROUP_ID)
+    # 沒有結果的情況
+    if len(results) == 0:
+        return "unknown"
+    result = results[0].as_dict()
+
+    # 找不到相像的人
+    if len(result["candidates"]) == 0:
+        return "unknown"
+    # 雖然有類似的人，但信心程度太低
+    if result["candidates"][0]["confidence"] < 0.5:
+        return "unknown"
+
+    person = FACE_CLIENT.person_group_person.get(PERSON_GROUP_ID,
+                                                 result["candidates"][0]["person_id"])
+
+    return person.name
+
+
+@HANDLER.add(MessageEvent, message=ImageMessage) # 檢查輸入是否為image
+def handle_content_message(event):
+
+    filename = "{}.jpg".format(event.message.id)
+    message_content = LINE_BOT.get_message_content(event.message.id)
+    with open(filename, "wb") as f_w:
+        for chunk in message_content.iter_content():
+            f_w.write(chunk)
+    f_w.close()
+    image = IMGUR_CLIENT.image_upload(filename, "first", "first")
+    #link = image["response"]["data"]["link"]
+    name = azure_face_recognition(filename)
+
+    if name != "":
+        now = datetime.now(timezone(timedelta(hours=8))).strftime("%Y-%m-%d %H:%M")
+        output = "{0}, {1}".format(name, now)
+    else:
+        output = "unknown"
+        output = TextSendMessage(text=output)
+
+    LINE_BOT.reply_message(event.reply_token,
+                           output)
